bb89d2427c44ffaab79e1839da2cc51db4604ce8
XanderDhondtUCLL <xander.dhondt@student.ucll.be>
2024-01-20 20:10:29 +0100

    added functionality to slee.py module

--- code/main.py
+++ code/main.py
@@ -1,3 +1,24 @@
 from slee import *
 from tkinter import *
 
+# setup
+
+window = Tk()
+window.geometry("420x420")
+window.title("slee.py")
+icon = PhotoImage(file='./data/sleepy.png')
+window.iconphoto(True, icon)
+window.config(background='#353839')
+
+# main
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
--- code/slee.py
+++ code/slee.py
@@ -1,14 +1,47 @@
+path = './data/notes.txt'
+
+# write the content of the task to the save file
 def writeNotes(content):
-    with open('.../data/notes.txt', 'a', encoding='utf-8') as FILE:
+    with open(path, 'a', encoding='utf-8') as FILE:
         FILE.write(content)
 
-def readNotes():
-    with open('.../data/notes.txt', 'r', encoding='utf-8') as FILE:
-        for line in FILE:
-            print(line.rstrip())
 
+# read content of the save file and return it in a list
+def readNotes():
+    returnList = []
+    with open('./data/notes.txt', 'r', encoding='utf-8') as FILE:
+        for line in FILE:
+            line = line.rstrip()
+
+            # Check if the line is whitespace
+            if not line:
+                returnList
+            # if not whitespace put value of line in the list to return
+            else:
+                returnList += [line]
+
+        # if the file is empty return None
+        if not returnList:
+            return
+        
+    return returnList
+
+# completely empty the save file
 def clearNotes():
-    with open('.../data/notes.txt', 'w', encoding='utf-8') as FILE:
+    with open(path, 'w', encoding='utf-8') as FILE:
         FILE.truncate(0)
 
-writeNotes('Nea')
+# return the length of a given file.    
+def parseLength():
+    count = 0
+    with open('./data/notes.txt', 'r', encoding='utf-8') as FILE:
+        for line in FILE:
+            line = line.rstrip()
+
+            # Check if the line is whitespace
+            if not line:
+                count
+            else:
+                count+=1
+
+    return count
