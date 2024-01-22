c4ff5d6fac2d32cf8097b99b3ff32b56cdaeee16
XanderDhondtUCLL <xander.dhondt@student.ucll.be>
2024-01-22 22:40:55 +0100

    added functionality for a popup widget

--- code/main.py
+++ code/main.py
@@ -1,19 +1,48 @@
 from tkinter import *
 from slee import *
 
-# setup #
-running = True
+# Setup
+background_color = '#191919'
+foreground_color = '#F5F5F5'
 
+# Function to update notes in the GUI
+def update_notes():
+    for note in readNotes():
+        label = Label(itemFrame, 
+                      text=note,
+                      wraplength=300,
+                      font='Arial',
+                      fg=foreground_color,
+                      bg=background_color)
+        label.pack(side=TOP, anchor=W)
+
+# Function to open popup window upon pressing add button
+def addPopup():
+    top = Toplevel(window)
+    top.geometry("420x200")
+    top.title("adding task")
+
+    label = Label(top, text='dit is een test!')
+    label.pack()
+
+
+# GUI setup
 window = Tk()
 window.geometry("420x420")
 window.title("slee.py")
-window.config(background="#191919")
+window.config(background=background_color)
 
-# main # 
-while running:
-    for i in readNotes():
-        content = i
-        label = Label(window, text=i)
-        label.pack()
+# Main
+itemFrame = Frame(window, background=background_color)
+itemFrame.pack(side=TOP, fill=BOTH, expand=YES)
 
-    window.mainloop()
+btnFrame = Frame(window)
+btnFrame.pack(side=BOTTOM, padx=10 ,pady=10)
+
+button = Button(btnFrame, text='Add note', command=addPopup)
+button.pack()
+
+# Update notes initially
+update_notes()
+
+window.mainloop()
